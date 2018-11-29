fe37a1bd107f3ca985a91a73450b11cd8410c924
Storn5 <storn5@ukr.net>
2018-11-29 12:57:39 +0200

    Optimized calculator (Program.cs)

--- Program.cs
+++ Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 namespace ConsoleApp1 {
     class Program {
+        static void PrintLine(string output) { Console.WriteLine(output); }
+        static double InputDouble() {
+            try { return double.Parse(Console.ReadLine()); }
+            catch { PrintLine("Error. Try again."); return 0.0; }
+        }
         static void Main(string[] args) {
             while (true) {
-                try {
-                    char oper = Console.ReadLine()[0];
-                    if (oper == 'q') { break; }
-                    double a = double.Parse(Console.ReadLine()), b = double.Parse(Console.ReadLine()), ans = 0;
-                    if (oper == '+') { ans = a + b; }
-                    else if (oper == '-') { ans = a - b; }
-                    else if (oper == '*') { ans = a * b; }
-                    else if (oper == '/') { ans = a / b; }
-                    else if (oper == '%') { ans = a % b; }
-                    else if (oper == '^') { ans = Math.Pow(a, b); }
-                    else { Console.WriteLine("Wrong operation. Try again."); }
-                    Console.WriteLine(a + " " + oper + " " + b + " = " + ans);
-                } catch { Console.WriteLine("Error. Try again."); }
+                char oper = Console.ReadLine()[0];
+                if (oper == 'q') { break; }
+                double a = InputDouble(), b = InputDouble(), ans = 0;
+                switch (oper) {
+                    case '+': ans = a + b;
+                        break;
+                    case '-': ans = a - b;
+                        break;
+                    case '*': ans = a * b;
+                        break;
+                    case '/': ans = a / b;
+                        break;
+                    case '%': ans = a % b;
+                        break;
+                    case '^': ans = Math.Pow(a, b);
+                        break;
+                    default: ans = 0;
+                        PrintLine("Wrong operation.");
+                        break;
+                }
+                PrintLine(a + " " + oper + " " + b + " = " + ans);
             }
         }
     }
